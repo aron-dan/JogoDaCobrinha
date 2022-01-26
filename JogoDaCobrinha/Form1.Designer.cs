99d45280bc1834176882390f4881662ff4ecc92c
Tester <Tester@DANIEL-PC>
2022-01-25 23:42:33 -0300

    Add project files.

---
+++ JogoDaCobrinha/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿
+namespace JogoDaCobrinha
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.startButtom = new System.Windows.Forms.Button();
+            this.snapButtom = new System.Windows.Forms.Button();
+            this.picCanvas = new System.Windows.Forms.PictureBox();
+            this.txtScore = new System.Windows.Forms.Label();
+            this.txtHighScore = new System.Windows.Forms.Label();
+            this.GameTime = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.picCanvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // startButtom
+            // 
+            this.startButtom.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.startButtom.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButtom.ForeColor = System.Drawing.Color.Black;
+            this.startButtom.Location = new System.Drawing.Point(610, 22);
+            this.startButtom.Name = "startButtom";
+            this.startButtom.Size = new System.Drawing.Size(107, 60);
+            this.startButtom.TabIndex = 0;
+            this.startButtom.Text = "START";
+            this.startButtom.UseVisualStyleBackColor = false;
+            this.startButtom.Click += new System.EventHandler(this.StartGame);
+            // 
+            // snapButtom
+            // 
+            this.snapButtom.BackColor = System.Drawing.Color.SpringGreen;
+            this.snapButtom.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.snapButtom.Location = new System.Drawing.Point(610, 98);
+            this.snapButtom.Name = "snapButtom";
+            this.snapButtom.Size = new System.Drawing.Size(107, 59);
+            this.snapButtom.TabIndex = 1;
+            this.snapButtom.Text = "SNAP";
+            this.snapButtom.UseVisualStyleBackColor = false;
+            this.snapButtom.Click += new System.EventHandler(this.TakeSnapShot);
+            // 
+            // picCanvas
+            // 
+            this.picCanvas.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.picCanvas.Location = new System.Drawing.Point(13, 22);
+            this.picCanvas.Name = "picCanvas";
+            this.picCanvas.Size = new System.Drawing.Size(580, 680);
+            this.picCanvas.TabIndex = 2;
+            this.picCanvas.TabStop = false;
+            this.picCanvas.Paint += new System.Windows.Forms.PaintEventHandler(this.UpdatePictureBoxGraphics);
+            // 
+            // txtScore
+            // 
+            this.txtScore.AutoSize = true;
+            this.txtScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtScore.Location = new System.Drawing.Point(599, 189);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(88, 24);
+            this.txtScore.TabIndex = 3;
+            this.txtScore.Text = "Score: 0";
+            // 
+            // txtHighScore
+            // 
+            this.txtHighScore.AutoSize = true;
+            this.txtHighScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtHighScore.Location = new System.Drawing.Point(599, 226);
+            this.txtHighScore.Name = "txtHighScore";
+            this.txtHighScore.Size = new System.Drawing.Size(113, 20);
+            this.txtHighScore.TabIndex = 4;
+            this.txtHighScore.Text = "HighScore: 0";
+            // 
+            // GameTime
+            // 
+            this.GameTime.Interval = 40;
+            this.GameTime.Tick += new System.EventHandler(this.GameTimerEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(748, 725);
+            this.Controls.Add(this.txtHighScore);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.picCanvas);
+            this.Controls.Add(this.snapButtom);
+            this.Controls.Add(this.startButtom);
+            this.Name = "Form1";
+            this.Text = "Jogo da Cobrinha";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.picCanvas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButtom;
+        private System.Windows.Forms.Button snapButtom;
+        private System.Windows.Forms.PictureBox picCanvas;
+        private System.Windows.Forms.Label txtScore;
+        private System.Windows.Forms.Label txtHighScore;
+        private System.Windows.Forms.Timer GameTime;
+    }
+}
+
---
+++ JogoDaCobrinha/Settings.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JogoDaCobrinha
+{
+    class Settings
+    {
+
+        public static int Widh { get; set; }
+        public static int Height { get; set; }
+        public static string direction;
+
+        public Settings()
+        {
+            Widh = 16;
+            Height = 16;
+            direction = "left";
+        }
+
+    }
+}
